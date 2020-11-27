0df154106f677bc547d8720c6fec9111ab0af056
eyroooon <aaronsanantonioparducho@gmail.com>
2020-11-27 21:36:25 +0800

    ang pogi mo 2

--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 const uniqueValidator = require('mongoose-unique-validator');
+//pogi
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
