8d7a21b076f2ca70e3f20183cf8bcfbe27523208
eyroooon <aaronsanantonioparducho@gmail.com>
2020-11-27 23:04:36 +0800

    store sdones2

--- server/models/products.js
+++ server/models/products.js
@@ -1,53 +1,54 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: true,
-    minlength: 3,
-  },
-  foodType: {
-    type: [String],
-    unique: true,
-    required: true,
-    minlength: 3,
-  },
-  description : {
-    type: String,
-    unique: true,
-    required: true,
-    minlength: 3,
-    },
-details: [{
-        size: {
-            type: String,
-            default: "default"
-        },
-        price: {
-            type: String,
-            required: true
-        }
-    }],
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    },
-    products: [{
-      type: mongoose.Schema.Types.ObjectId,
-    ref: 'products',
-  }]
-});
+// const productSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     unique: true,
+//     required: true,
+//     minlength: 3,
+//   },
+//   foodType: {
+//     type: [String],
+//     unique: true,
+//     required: true,
+//     minlength: 3,
+//   },
+//   description : {
+//     type: String,
+//     unique: true,
+//     required: true,
+//     minlength: 3,
+//     },
+//     details: [{
+//         type: Object,
+//         size: {
+//             type: String,
+//             default: "default"
+//         },
+//         price: {
+//             type: String,
+//             required: true
+//         }
+//     }],
+//   },
+//   user: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User',
+//     },
+//     products: [{
+//       type: mongoose.Schema.Types.ObjectId,
+//     ref: 'products',
+//   }]
+// });
 
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
+// blogSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString();
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//   },
+// });
 
-module.exports = mongoose.model('Store', storeSchema);
+// module.exports = mongoose.model('Store', storeSchema);
